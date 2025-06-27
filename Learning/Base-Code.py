9372f0ca0b2a8dcfa4e592272c02cf3d83a321d4
Mahdi KeyhanFar <kiyanknf@gmail.com>
2025-06-27 14:39:21 +0330

    Upload Base-Code.py

---
+++ Learning/Base-Code.py
@@ -0,0 +1,13 @@
+import customtkinter as ctk # ==> For import CustomTkinter and make a object from it (ctk)
+
+root = ctk.CTk() # ==> main form
+root.title("mahdi-keyhanfar") # ==> title of form
+root.geometry("400x300")  # ==> size of form (WxH)
+
+'''
+
+Coding In This Place 
+
+'''
+
+root.mainloop() # ==> mainloop to keep open app
