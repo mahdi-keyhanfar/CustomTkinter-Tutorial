422cf0587be1a0d050bd813b40e38a430fb0fd25
Mahdi KeyhanFar <kiyanknf@gmail.com>
2025-06-27 16:05:18 +0330

    Add button py file

---
+++ Learning/CTk-Widgets/1-CTkButton.py
@@ -0,0 +1,16 @@
+import customtkinter as ctk # ==> For import CustomTkinter and make a object from it (ctk)
+
+root = ctk.CTk()
+root.title("mahdi-keyhanfar")
+root.geometry("400x300")
+
+def btn_click():
+    print("Button Clicked !")
+
+#!  CTkButton
+button = ctk.CTkButton(root,
+                        text = "Button1", # ==> Button Name
+                        command = btn_click) # ==> Connect Def To Button
+button.pack() # ==> show button
+
+root.mainloop()
