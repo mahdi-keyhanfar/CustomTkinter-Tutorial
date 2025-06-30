a4e1812823a410bc0b2e46c7a1d7eedfd37b3a6d
Mahdi KeyhanFar <kiyanknf@gmail.com>
2025-06-30 23:24:56 +0330

    Update 3-CTkComboBox.py

--- Learning/CTk-Widgets/3-CTkComboBox.py
+++ Learning/CTk-Widgets/3-CTkComboBox.py
@@ -9,10 +9,10 @@ def combobox_callback(choice):
 
 #!  CTkComboBox
 combobox = ctk.CTkComboBox(root,
-                            values=["option 1", "option 2"],
-                            command=combobox_callback)
+                            values=["option 1", "option 2"], # ==> Values Of ComboBox
+                            command=combobox_callback) # ==> Connect Def To ComboBox
 
-combobox.set("option 2")
+combobox.set("option 2") # ==> Select a default Value Or Option For ComboBox
 
 combobox.pack(pady=20) #==> pady=top and bottom ---- padx=left and right
 
