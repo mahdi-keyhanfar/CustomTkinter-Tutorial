ec72fed7a748c1fc4f121a933750f36af3a3f8c7
Mahdi KeyhanFar <kiyanknf@gmail.com>
2025-07-23 15:23:05 +0330

    Add CTkLable.py

--- Learning/CTk-Widgets/5-CTkLable.py
+++ Learning/CTk-Widgets/5-CTkLable.py
@@ -7,6 +7,7 @@ root.geometry("400x300")
 label = ctk.CTkLabel(root, text="CTkLabel",
                     fg_color="#f00")
 label.pack(pady = 20) #==> pady=top and bottom ---- padx=left and right
+
 root.mainloop()
 
 # For Learn More Arguments Of Button Read README.md in Repositories Main Page
