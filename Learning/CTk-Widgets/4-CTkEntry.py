627be7ebee2221891be222e93c13e74037951ffe
Mahdi KeyhanFar <kiyanknf@gmail.com>
2025-07-23 14:56:52 +0330

    Add CTkEntry.py

---
+++ Learning/CTk-Widgets/4-CTkEntry.py
@@ -0,0 +1,13 @@
+import customtkinter as ctk # ==> For import CustomTkinter and make a object from it (ctk)
+
+root = ctk.CTk()
+root.title("mahdi-keyhanfar")
+root.geometry("400x300")
+
+entry = ctk.CTkEntry(root,
+                      placeholder_text="CTkEntry") # ==> For Show This Entry For Whath (PlaceHolder Of Entry )
+entry.pack(pady = 20) #==> pady=top and bottom ---- padx=left and right
+
+root.mainloop()
+
+# For Learn More Arguments Of Button Read README.md in Repositories Main Page
