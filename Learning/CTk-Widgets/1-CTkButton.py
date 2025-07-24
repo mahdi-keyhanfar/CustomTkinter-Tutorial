b78131ecc153c4fa7d075f29ff2cc9d733b8a6e2
Mahdi KeyhanFar <kiyanknf@gmail.com>
2025-07-24 12:40:33 +0330

    Add CTkButton.py

--- Learning/CTk-Widgets/1-CTkButton.py
+++ Learning/CTk-Widgets/1-CTkButton.py
@@ -7,10 +7,9 @@ root.geometry("400x300")
 def btn_click():
     print("Button Clicked !")
 
-#!  CTkButton
 button = ctk.CTkButton(root,
-                        text = "Button1", # ==> Button Name
-                        command = btn_click) # ==> Connect Def To Button
+                      text = "Button1", # ==> Button Name
+                      command = btn_click) # ==> Connect Def To Button
 
 button.pack(pady = 20) #==> pady=top and bottom ---- padx=left and right
 
