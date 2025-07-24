22790407fa58422132f74f520ee83f8429befd8c
Mahdi KeyhanFar <kiyanknf@gmail.com>
2025-07-24 12:35:23 +0330

    Add CTkEntry.py

--- Learning/CTk-Widgets/4-CTkEntry.py
+++ Learning/CTk-Widgets/4-CTkEntry.py
@@ -5,7 +5,7 @@ root.title("mahdi-keyhanfar")
 root.geometry("400x300")
 
 entry = ctk.CTkEntry(root,
-                      placeholder_text="CTkEntry") # ==> For Show This Entry For Whath (PlaceHolder Of Entry )
+                    placeholder_text="CTkEntry") # ==> For Show This Entry For Whath (PlaceHolder Of Entry )
 entry.pack(pady = 20) #==> pady=top and bottom ---- padx=left and right
 
 root.mainloop()
